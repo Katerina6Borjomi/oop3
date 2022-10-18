ca13db490813b7b01894a13bec3749f9f52fbfff
Katerina6Borjomi <106556254+Katerina6Borjomi@users.noreply.github.com>
2022-10-18 12:48:58 +0300

    Add files via upload

--- functions.cpp
+++ functions.cpp
@@ -35,5 +35,98 @@ namespace oop3{
         this->fact = fact1;
     }
 
+    void Vector_3::print_vector(){
+        for(int i = 0; i < this->fact; i++){
+            cout<< this->vector[i] << endl;
+        }
+    }
+
+    int Vector_3::positiveness(){
+        int f = 0;
+        for(int i = 0; i < this->fact; i++){
+            if(vector[i] == 'X'){
+                f = 1;
+            }
+        }
+        return f;//1 - X; 0 - нет
+    }
+
+    Vector_3 Vector_3::and_vectors(oop3::Vector_3* b){
+        Vector_3 res_vec;
+        int n1, n2, n;
+        n1 = this->fact;
+        n2 = b->fact;
+        if(n1 > n2){
+            n = n1;
+        }else{
+            n = n2;
+        }
+        char a;
+        char c;
+        for(int i = 0; i < n; i++){
+            if(i > n1){
+                a = 'X';
+                c = b->vector[i];
+            }else if(i > n2){
+                c = 'X';
+                a = this->vector[i];
+            }else{
+                a = this->vector[i];
+                c = b->vector[i];
+            }
+            if(a == '1'){
+                res_vec.vector[i] = c;
+            }else if(a == '0'){
+                res_vec.vector[i] = '0';
+            }else{
+                if(c == '1'){
+                    res_vec.vector[i] = 'X';
+                }else if(c == '0'){
+                    res_vec.vector[i] = '0';
+                }else{
+                    res_vec.vector[i] = 'X';
+                }
+            }
+        }
+        return res_vec;
+    }
+
+    Vector_3 Vector_3::or_vectors(oop3::Vector_3* b){
+        Vector_3 res_vec;
+        int n1, n2, n;
+        n1 = this->fact;
+        n2 = b->fact;
+        if(n1 > n2){
+            n = n1;
+        }else{
+            n = n2;
+        }
+        char a;
+        char c;
+        for(int i = 0; i < n; i++){
+            if(i > n1){
+                a = 'X';
+                c = b->vector[i];
+            }else if(i > n2){
+                c = 'X';
+                a = this->vector[i];
+            }else{
+                a = this->vector[i];
+                c = b->vector[i];
+            }
+            if(a == '0'){
+                res_vec.vector[i] = c;
+            }else if(a == '1'){
+                res_vec.vector[i] = '1';
+            }else{
+                if(c == '1'){
+                    res_vec.vector[i] = '1';
+                }else{
+                    res_vec.vector[i] = 'X';
+                }
+            }
+        }
+        return res_vec;
+    }
 
 }
--- functions.h
+++ functions.h
@@ -19,9 +19,12 @@ namespace oop3{
                     for(int i = 1; i < 1000; i++){
                         vector[i] = 0;
                     }
-                }else if((fact1 != 0) && (mass == nullptr)){
+                }else if(mass == nullptr){
                     for(int i = 0; i < fact1; i++){
                         vector[i] = 'X';
+                    }
+                    for(int j = fact1; j < 1000; j++){
+                        vector[j] = 0;
                     }
                 }else{
                     for(int i = 0; i < strlen(mass); i++){
@@ -32,10 +35,14 @@ namespace oop3{
 
             int get_fact();
             void set_fact(int fact1);
-            Vector_3 and_vectors(Vector_3 b);
-            Vector_3 or_vectors(Vector_3 b);
+            Vector_3 and_vectors(Vector_3* b);
+            Vector_3 or_vectors(Vector_3* b);
             void print_vector();
-            int cmp_vectors(Vector_3 b);
+            Vector_3 cmp_vectors_equel(Vector_3* b);
+            Vector_3 cmp_vectors_bigger(Vector_3* b);
+            Vector_3 cmp_vectors_smaller(Vector_3* b);
+            Vector_3 cmp_vectors_sm_eq(Vector_3* b);
+            Vector_3 cmp_vectors_bg_eq(Vector_3* b);
             Vector_3 invert_vector();
             int positiveness();
     };
